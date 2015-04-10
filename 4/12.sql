f6d8fd5ad9db878f4ad5580cd289834ef8d8e09f
ulmusic <ulmusic@users.noreply.github.com>
2015-04-10 12:54:05 -0400

    finished 11, on to 12 (which was actually added now)

---
+++ 4/12.sql
@@ -0,0 +1,9 @@
+/* When looking at the Top 30 Scorers list over two different download dates, a
+ * positive upset for a team is when the number of top scorers from the team
+ * increases (e.g., from 1 to 2), whereas a negative upset is when the number
+ * of top scorers from the team decreases (e.g., from 1 to 0). For every team,
+ * show how many positive upsets it had over time (i.e., using all the data in
+ * the database). Note that you should only use pairs of consecutive dates to
+ * make these comparisons.  (Hint: use views to simplify the query)
+ */
+
